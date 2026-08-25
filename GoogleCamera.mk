8e86ca7697c30efde3ae14f377a235c21a501efe
n0escape-exe <viaan4542@gmail.com>
2026-08-25 14:58:28 +0000

    hardware: GoogleCamera: Import Proprietary Blobs
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

---
+++ GoogleCamera.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlay
+PRODUCT_PACKAGES += \
+    GoogleCameraOverlay
+
+
+# GoogleCamera
+PRODUCT_PACKAGES += \
+    GoogleCamera
